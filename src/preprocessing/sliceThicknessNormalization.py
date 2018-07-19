da84958325852ef40a42a65c3961d2a6d56870f5
vascobailao <vasco.bailao@gmail.com>
2018-07-19 19:42:57 +0100

    preprocessing
    preprocessing - slice thickness

---
+++ src/preprocessing/sliceThicknessNormalization.py
@@ -0,0 +1,18 @@
+import numpy as np
+import scipy
+
+
+def resample(image, scan, new_spacing=[1, 1, 1]):
+    # Determine current pixel spacing
+    spacing = map(float, ([scan[0].SliceThickness] + scan[0].PixelSpacing))
+    spacing = np.array(list(spacing))
+
+    resize_factor = spacing / new_spacing
+    new_real_shape = image.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / image.shape
+    new_spacing = spacing / real_resize_factor
+
+    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
+
+    return image, new_spacing
