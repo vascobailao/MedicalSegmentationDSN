f421a041e4b847de34b7d9de922f8cd55e2f573f
vascobailao <vasco.bailao@gmail.com>
2018-07-27 18:11:37 +0100

    update
    muitpleExperts

---
+++ src/preprocessing/FuseMultipleExperts.py
@@ -0,0 +1,14 @@
+
+
+
+class FuseMultipleExperts():
+
+    def __init__(self, me):
+        self.me = me
+
+
+    def self_consistency_score(self):
+        return 0
+
+    def MRF(self):
+        return 0
