31cfa4fe59a9c37043917e17f121119e38b26d2f
vascobailao <vasco.bailao@gmail.com>
2018-07-30 19:06:41 +0100

    update

--- src/preprocessing/FuseMultipleExperts.py
+++ src/preprocessing/FuseMultipleExperts.py
@@ -1,10 +1,17 @@
-
+import cv2
 
 
 class FuseMultipleExperts():
 
-    def __init__(self, me):
-        self.me = me
+    def __init__(self, image, small_cropping_size, big_cropping_size):
+        self.small_cropping_size = small_cropping_size
+        self.big_cropping_size = big_cropping_size
+        self.image = image
+
+
+    def small_cropping(self):
+        image = cv2.imread(self.image)
+
 
 
     def self_consistency_score(self):
