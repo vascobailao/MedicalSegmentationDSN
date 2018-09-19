7452806b328d08bce048eed375e3d52b64ef07be
vascobailao <vasco.bailao@gmail.com>
2018-09-19 14:21:58 +0100

    update

--- src/network/layers/Deconv_layer.py
+++ src/network/layers/Deconv_layer.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 class Deconv_layer():
 
-    def __init__(self, layer_input, filter, output_shape, strides, w, b):
+    def __init__(self, layer_input, filter, output_shape, strides, w, b, initializer):
         self.layer_input = layer_input
         self.filter = filter
         self.output_shape = output_shape
@@ -10,6 +10,7 @@ class Deconv_layer():
         self.padding = 'SAME'
         self.weights = w
         self.biases = b
+        self.initializer = initializer
 
 
     def deconvolution_layer_3d(self):
@@ -17,7 +18,7 @@ class Deconv_layer():
         assert len(self.strides) == 5  # must match input dimensions [batch, depth, height, width, in_channels]
         assert self.padding in ['VALID', 'SAME']
         # w = tf.Variable(initial_value=tf.truncated_normal(shape=filter), name='weights')
-        self.w = tf.Variable(initial_value=xavier_uniform_dist_conv3d(shape=filter), name='weights')
-        self.b = tf.Variable(tf.constant(1.0, shape=[filter[-2]]), name='biases')
+        self.w = tf.Variable(initial_value=self.initializer(shape=self.filter), name='weights')
+        self.b = tf.Variable(tf.constant(1.0, shape=[self.filter[-2]]), name='biases')
         deconvolution = tf.nn.conv3d_transpose(self.layer_input, self.w, self.output_shape, self.strides, self.padding)
         return deconvolution + self.b
